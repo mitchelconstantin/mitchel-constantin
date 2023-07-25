5cbc5e22a77bf1ff389145af43e4ae0a873b3650
Mitchel <mitchconstantin@gmail.com>
2023-07-24 21:10:03 -0400

    font size and typo fix

--- src/components/about-me.tsx
+++ src/components/about-me.tsx
@@ -7,10 +7,10 @@ export const AboutMe = () => {
       colorScheme="blue"
       style={{ padding: "8px " }}
       width="100%"
-      fontSize={13}
+      fontSize={13.8}
     >
       Hello, I'm Mitchel Constantin, a full-stack web developer passionate about
-      seamless digital experiences. When I'm not coding, you'll find me I
+      seamless digital experiences. When I'm not coding, you'll find me
       exploring the world of finance, savoring a cup of coffee, and enjoying
       time with my adorable cats. Let's connect and explore how my skills can
       contribute to your team's success.
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-// import "./index.css";
 import {
   ChakraProvider,
   extendTheme,
   type ThemeConfig,
 } from "@chakra-ui/react";
-
 import { ColorModeScript } from "@chakra-ui/react";
 
-// 2. Add your color mode config
 const config: ThemeConfig = {
   initialColorMode: "system",
   useSystemColorMode: true,
